739852386d66e9f8ee046885660f13abe2ec4d45
Rathees Koneswaran <ratheesreo@gmail.com>
2024-07-25 17:04:49 +0530

    feat: add header component

---
+++ components/layout/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo isset($pageTitle) ? $pageTitle . " | ISD" : "Ideal Staging and Design"; ?></title>
+    <meta
+        content="<?php echo isset($metaDescription) ? $metaDescription : "Transforming properties into stunning, market-ready spaces that captivate potential buyers and drive successful sales."; ?>"
+        name="description">
+
+    <!-- Google / Search Engine Tags -->
+    <meta itemprop="name" content="<?php echo isset($metaTitle) ? $metaTitle : "Ideal Staging and Design"; ?>">
+    <meta itemprop="description"
+        content="<?php echo isset($metaDescription) ? $metaDescription : "Transforming properties into stunning, market-ready spaces that captivate potential buyers and drive successful sales."; ?>">
+    <meta itemprop="image" content="https://idealstaginganddesign.com/images/Slider/3.jpg">
+
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://idealstaginganddesign.com">
+    <meta property="og:title" content="<?php echo isset($metaTitle) ? $metaTitle : "Ideal Staging and Design"; ?>">
+    <meta property="og:description"
+        content="<?php echo isset($metaDescription) ? $metaDescription : "Transforming properties into stunning, market-ready spaces that captivate potential buyers and drive successful sales."; ?>">
+    <meta property="og:image" content="https://idealstaginganddesign.com/images/Slider/3.jpg">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://idealstaginganddesign.com">
+    <meta property="twitter:title" content="<?php echo isset($metaTitle) ? $metaTitle : "Ideal Staging and Design"; ?>">
+    <meta property="twitter:description"
+        content="<?php echo isset($metaDescription) ? $metaDescription : "Transforming properties into stunning, market-ready spaces that captivate potential buyers and drive successful sales."; ?>">
+    <meta property="twitter:image" content="https://idealstaginganddesign.com/images/Slider/3.jpg">
+
+    <!-- Canonical -->
+    <link rel="canonical" href="https://idealstaginganddesign.com/index.html">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="images/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Font -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/elegant-font.css">
+
+    <!-- REVOLUTION STYLE SHEETS -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/settings.css">
+    <!-- REVOLUTION LAYERS STYLES -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+    <!-- REVOLUTION NAVIGATION STYLES -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">
+    <!-- OWL CAROUSEL
+	  	================================================== -->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <!-- SCROLL BAR MOBILE MENU
+  		================================================== -->
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css" />
+
+    <!-- Main Style -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
