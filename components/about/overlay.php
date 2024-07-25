2d1cfbe0895f1df09e9ae979ab610b3736fd5e32
Rathees Koneswaran <ratheesreo@gmail.com>
2024-07-25 23:02:55 +0530

    fix: update overlay

--- components/about/overlay.php
+++ components/about/overlay.php
@@ -2,7 +2,7 @@
     <div class="overlay-arc overlay-3">
         <div class="layer-1 text-center">
             <div class="box-content-overlay-3">
-                <h2>We are creative interior design, architecture and<br> decor your space</h2>
+                <h2>Transforming spaces, igniting imaginations – where every room becomes a dream home.</h2>
                 <a href="contact.php" class="ot-btn btn-sub-color btn-long text-cap">Work with us</a>
             </div>
         </div>
